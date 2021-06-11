0bff7bd155393e89baa799c6291a254879b865ff
Arnaud <arnaudg9806@gmail.com>
2021-06-11 10:04:59 +0200

    [WIP] loggin register

---
+++ src/js/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Register } from './pages/Register'
+import { Loggin } from './pages/Loggin'
+export const App = () => {
+
+    return (
+        <div>
+            <Register />
+            <Loggin/>   
+        </div>
+        
+    )
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { App } from './app'
 const MOUNT_NODE = document.getElementById('app')
 
 ReactDOM.render(
     <div>
-        <h1>Hello</h1>
+        <App/>
     </div>,MOUNT_NODE
 )
